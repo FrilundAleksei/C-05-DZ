e2d2f5741aac07e70107a5dd5b5cbf65a8a2c851
FrilundAleksei <frilundag@gmail.com>
2022-08-20 13:50:05 +0300

    решена задача 038

--- task038/Program.cs
+++ task038/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+using static System.Console;
+Clear();
+
+
+WriteLine("Введите размер массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] NewArray = GetArrey(n);
+PrintArray(NewArray);
+WriteLine();
+
+int min = 0;
+int max = 0;
+foreach (int i in NewArray)
+{
+    if (min > i) min = i;
+    if (max < i) max = i;
+}
+
+WriteLine($"\nРазница между между максимальным ({max}) и минимальным({min}) элементами массива: {max-min}");
+
+
+
+int [] GetArrey (int n)
+{
+int [] array = new int[n];
+for (int i = 0;  i<n; i++)
+    {
+       array [i]= new Random().Next (-100, 100);  
+    }
+       return array;
+       }
+
+void PrintArray(int[] Mass)
+{
+    Write("[");
+    for (int i = 0;  i<Mass.Length; i++)
+    {
+      Write (Mass[i] + ", ");
+    }
+    Write("]");
+}
