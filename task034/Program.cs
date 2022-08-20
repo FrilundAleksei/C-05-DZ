3411f94ea2de8faba18c07de0e8208b7fca4b165
FrilundAleksei <frilundag@gmail.com>
2022-08-20 11:05:49 +0300

    решена задача 034

--- task034/Program.cs
+++ task034/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+using static System.Console;
+Clear();
+
+WriteLine("Какое число цыфр должно быть в массиве?");
+int size = Convert.ToInt32(ReadLine());
+
+int [] NewArray = GetArrey(size);
+WriteLine($"[{String.Join(", ",NewArray)}]");
+
+int count = 0;
+for (int i = 0; i < NewArray.Length; i++)
+{
+    if(NewArray[i]%2==0)
+    {
+        count+=1;
+    }
+}
+   Console.WriteLine($"В массиве всего {NewArray.Length} чисел, {count} из них чётные.");
+
+
+
+
+
+int [] GetArrey (int N)
+{
+int [] array = new int[N];
+for (int i = 0;  i<N; i++)
+    {
+       array [i]= new Random().Next (100, 1000);  
+    }
+       return array;
+ }
+
+
+
+
+
+
